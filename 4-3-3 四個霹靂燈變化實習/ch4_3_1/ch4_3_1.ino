34c4e7a7d2836b89e1d671863b5c329e4a71355d
moonlight76856e4 <moonlight76856e4@users.noreply.github.com>
2020-04-04 21:43:06 +0800

    20200404

---
+++ 4-3-3 四個霹靂燈變化實習/ch4_3_1/ch4_3_1.ino
@@ -0,0 +1,47 @@
+/*
+ *四個霹靂燈變化實習
+ *
+ */
+const int led[] = {10,11,12,13};      //LED連接至數位輸出接腳10~13(由左至右移)
+int i;                                //LED索引值
+int j = 0;                            //count計算
+int k = 0;                            //LED索引值
+int direct=0;                         //LED移位方向控制
+void setup()
+{
+    for(i=0;i<4;i++)                  //四個LED
+    {
+        pinMode(led[i], OUTPUT);      //設定數位接腳10~13為輸出模式
+    }
+}
+void loop()
+{
+    for(i=0;i<4;i++)                  //四個LED
+    {
+        digitalWrite(led[i],LOW);     //設定數位接腳10~13號數位腳輸出狀態為LOW(設定關閉所有LED)
+    }
+    for(j=0;j<i;j++)                  //由j來計數
+    {
+        digitalWrite(led[k],HIGH);    //設定第j號數位腳輸出狀態為HIGH(點亮第j個LED)
+        delay(500);                   //延遲0.5秒
+        digitalWrite(led[k],LOW);     //設定第j號數位腳輸出狀態為LOW(關閉第j個LED)
+        delay(500);                   //延遲0.5秒
+        digitalWrite(led[k],HIGH);    //設定第j號數位腳輸出狀態為HIGH(點亮第j個LED)
+        delay(500);                   //延遲0.5秒
+        digitalWrite(led[k],LOW);     //設定第j號數位腳輸出狀態為LOW(關閉第j個LED)
+        if(direct==0)                 //如LED往右移
+        {
+          if(k==4)                    //如已右移至最右方
+            direct=1;                 //改為左移
+          else                        //若尚未移至最右方
+             k=k+1;                   //繼續右移
+        }
+        else                          //若LED往左移
+       {
+         if(k==0)                     //如LED移至最左方
+            direct=0;                 //改為右移
+         else                         //若尚未移至最左方
+             k=k-1;                   //繼續左移
+        }
+    }
+}
